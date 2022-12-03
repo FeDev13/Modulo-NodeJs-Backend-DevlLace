93575ab9cd0e1c71f170d18dfcd0eecc7e5f9162
Federico Robledo <federicorobledo85@gmail.com>
2022-12-03 14:32:06 -0300

    agrega joi

--- modelos/usuarios.js
+++ modelos/usuarios.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-const usuarios = mongoose.Schema({
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const usuarios = new mongoose.Schema({
   nombre: {
     type: String,
     required: true,
@@ -10,4 +13,19 @@ const usuarios = mongoose.Schema({
   },
 });
 
+usuarios.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
 module.exports = mongoose.model("usuarios", usuarios);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    nombre: Joi.string().required().label("First Name"),
+    password: passwordComplexity().required().label("Password"),
+  });
+  return schema.validate(data);
+};
