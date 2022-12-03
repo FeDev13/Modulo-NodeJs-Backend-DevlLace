88d4e72483a703d060db5d3b3c1e3537bcad4292
Federico Robledo <federicorobledo85@gmail.com>
2022-12-03 20:10:05 -0300

    agrega ruta usuario con bcrypt

--- modelos/usuarios.js
+++ modelos/usuarios.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const usuarios = mongoose.Schema({
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const usuarios = new mongoose.Schema({
   nombre: {
     type: String,
     required: true,
@@ -10,4 +14,21 @@ const usuarios = mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("usuarios", usuarios);
+usuarios.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+const User = mongoose.model("usuarios", usuarios);
+
+const validate = (data) => {
+  const schema = Joi.object({
+    nombre: Joi.string().required().label("nombre"),
+    password: passwordComplexity().required().label("password"),
+  });
+  return schema.validate(data);
+};
+
+module.exports = { User, validate };
--- rutas/users.js
+++ rutas/users.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
-const User = require("../modelos/usuarios");
+const { User, validate } = require("../modelos/usuarios");
 
 /* metodo post de usuario */
-router.post("/user", (request, response) => {
-  //el home es users del middleware
-  const usuario = new User({
-    // agarra las propiedades del body del objeto
-    nombre: request.body.nombre,
-    password: request.body.password,
-  });
-  usuario
-    .save()
-    .then((data) => {
-      response.json(data);
-    })
-    .catch((error) => {
-      response.json({ message: error });
-    });
+router.post("/user", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+
+    const user = await User.findOne({ nombre: req.body.nombre });
+    if (user)
+      return res
+        .status(409)
+        .send({ message: "User with given email already Exist!" });
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    await new User({ ...req.body, password: hashPassword }).save();
+    res.status(201).send({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 /* metodo get de usuarios */
